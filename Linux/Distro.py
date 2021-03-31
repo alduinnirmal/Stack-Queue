dc308b8b047ce624e5d17ee1ce1f0e86c97d6a2d
Nirmal Shaju <61916346+alduinnirmal@users.noreply.github.com>
2021-03-31 08:41:38 +0530

    Update Distro.py

--- Linux/Distro.py
+++ Linux/Distro.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python
-#
-# Archey [version 0.2.8]
-#
-# Archey is a system information tool written in Python.
-#
-# Maintained by Melik Manukyan <melik@archlinux.us>
-# ASCII art by Brett Bohnenkamper <kittykatt@silverirc.com>
-# Changes Jerome Launay <jerome@projet-libre.org>
-# Fedora support by YeOK <yeok@henpen.org>
 
-# Distributed under the terms of the GNU General Public License v3.
-# See http://www.gnu.org/licenses/gpl.txt for the full license text.
-
-# Import libraries
 import os, sys, subprocess, optparse, re, linecache
 from subprocess import Popen, PIPE
 from optparse import OptionParser
